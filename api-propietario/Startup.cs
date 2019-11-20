5eb7445f317e006e8b265d120fd140f92cc01258
Eric <eric.rdn@gmail.com>
2019-11-19 22:48:55 -0300

    Ajustes nas API's

--- api-propietario/Startup.cs
+++ api-propietario/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace api_multas
 {
@@ -26,6 +27,11 @@ namespace api_multas
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,7 +44,14 @@ namespace api_multas
 
             app.UseHttpsRedirection();
 
+            app.UseSwagger();
+
             app.UseRouting();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+            });
 
             app.UseAuthorization();
 
